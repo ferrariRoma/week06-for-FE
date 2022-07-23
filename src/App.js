9b95b7b4a1f0c0a629581f53f14876c1ef94e781
FerrariRoma <win9592@gmail.com>
2022-07-23 13:53:33 +0900

    Update: Header default style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homs";
+import { StyledHeader, StyledSidebar } from "./components/styled";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledHeader>
+        <div>
+          <div>로고</div>
+          <div>오다 주웠다</div>
+          <div>가다 흘렸다</div>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUser} style={{ fontSize: "3rem" }} />
+        </div>
+      </StyledHeader>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
