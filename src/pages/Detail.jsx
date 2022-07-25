d5375de8883e652ff9f82f9bda872ba2a054ef9a
FerrariRoma <win9592@gmail.com>
2022-07-25 17:57:32 +0900

    Update: get existing Component
    just save to make new working branch

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,66 @@
 import React from "react";
+import {
+  StyledPostContainer,
+  StyledPostInnerContainer,
+  StylePostBtn,
+} from "../components/styled";
+import { useParams, useLocation } from "react-router-dom";
 
 const Detail = () => {
+  const params = useParams();
+  const location = useLocation();
+  console.log(params);
+  console.log(location);
   return (
     <>
-      <div>게시판 입니다</div>
+      {/* <StyledPostContainer>
+        <StyledPostInnerContainer>
+          {fileImage && (
+            <img
+              alt=""
+              src={fileImage.preview_url}
+              style={{ width: "39.9rem", height: "25rem" }}
+            ></img>
+          )}
+          <input
+            name="imageUpload"
+            type="file"
+            accept="image/*"
+            onChange={saveFileImage}
+            ref={(refParam) => (inputRef = refParam)}
+            style={{ display: "none" }}
+            onClick={(e) => (e.target.value = null)}
+          ></input>
+          <StylePostBtn>
+            <button onClick={() => inputRef.click()}>파일 찾기</button>
+            <button
+              style={{ width: "5rem", height: "3rem" }}
+              onClick={deleteFileImage}
+            >
+              이미지 삭제
+            </button>
+          </StylePostBtn>
+          제목
+          <input type="text" style={{ height: "2.5rem" }} ref={title}></input>
+          내용
+          <input
+            type="textarea"
+            style={{ height: "15rem" }}
+            ref={content}
+          ></input>
+          <StylePostBtn>
+            <button
+              onClick={() => {
+                sendImageToServer();
+                sendContentToServer();
+              }}
+            >
+              작성하기
+            </button>
+            <button onClick={() => navigate("/")}>취소</button>
+          </StylePostBtn>
+        </StyledPostInnerContainer>
+      </StyledPostContainer> */}
     </>
   );
 };
