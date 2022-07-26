192df48d5dd73e9cd8fc517954bc2b38d37bf80f
FerrariRoma <win9592@gmail.com>
2022-07-27 05:11:40 +0900

    Update: working css at comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { CommentBox } from "./styled";
+import { useSelector } from "react-redux";
 
 const Comment = (props) => {
+  const checked__comment = useSelector((state) => state);
   return (
     <>
       <CommentBox>
         <div className="contentDetail">
-          <div className="contentDetail__username">{props.info.nickname}</div>
-          <div className="contentDetail__timeStamp">{props.info.createdAt}</div>
-          <div className="contentDetail__editBtn">
+          <div className="contentDetail__username__time">
+            <div className="contentDetail__username">{props.info.nickname}</div>
+            <div className="contentDetail__timeStamp">
+              {props.info.createdAt}
+            </div>
+          </div>
+          {/* <div className="contentDetail__editBtn">
             <button>수정</button>
             <button>삭제</button>
-          </div>
+          </div> */}
+          {checked__comment === props.info.nickname ? (
+            <>
+              <div className="contentDetail__editBtn">
+                <button>수정</button>
+                <button>삭제</button>
+              </div>
+            </>
+          ) : null}
         </div>
-        <div>{props.info.comment}</div>
+        <div className="comment__content">{props.info.comment}</div>
       </CommentBox>
     </>
   );
