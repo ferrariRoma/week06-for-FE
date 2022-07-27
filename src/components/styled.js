eb787481c9026d0c0226ab15a8d4d11e05a3808c
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-07-28 00:48:15 +0900

    merge전 수정

--- src/components/styled.js
+++ src/components/styled.js
@@ -101,47 +101,6 @@ export const StyledPageLi = styled.li`
     background-color: rgba(38, 58, 108, 0.6);
   }
 `;
-// export const StyledPostContainer = styled.div`
-//   margin-top: 8rem;
-//   margin-bottom: 2rem;
-//   position: relative;
-//   top: 1rem;
-//   left: 50%;
-//   transform: translate(-50%, 0);
-//   border: transparent;
-//   border-radius: 1rem;
-//   box-shadow: 5px 5px 5px 5px lightgray;
-
-//   @media (max-width: 575px) {
-//     overflow: auto;
-//     width: 300px;
-//   }
-
-//   @media (min-width: 576px) and (max-width: 767px) {
-//     overflow: auto;
-//     width: 500px;
-//   }
-
-//   @media (min-width: 768px) and (max-width: 991px) {
-//     width: 700px;
-//     overflow: auto;
-//   }
-
-//   @media (min-width: 992px) and (max-width: 1199px) {
-//     width: 900px;
-//     overflow: auto;
-//   }
-
-//   @media (min-width: 1200px) {
-//     width: 1100px;
-//     overflow: auto;
-//   }
-// `;
-
-// export const StyledPostInnerContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-// `;
 
 export const StyledPostBtn = styled.div`
   display: flex;
@@ -191,4 +150,17 @@ export const StyledInputContent = styled.textarea`
   outline: none;
   box-sizing: border-box;
   padding: 1rem;
+`;
+
+
+export const StyledIntroContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-image: url("https://mblogthumb-phinf.pstatic.net/20150430_83/croxriver_1430364615321VA6xy_JPEG/DSC_3994.jpg?type=w800");
+  background-repeat: no-repeat;
+  background-position: top center;
+  background-size: cover;
+  background-attachment: fixed;
+  background-blend-mode : darken;
 `;
